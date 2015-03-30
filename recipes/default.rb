9035c9e4b1823005fe8fbfa35db3253d6a770690
Alex Freidah <afreidah@optoro.com>
2015-03-30 15:50:35 -0400

    foodcritic hates me

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,8 @@
 include_recipe 'zfs_linux'
 
-if node['optoro_zfs']['zfs_arc_max']
-  template '/etc/modprobe.d/zfs-arc-max.conf' do
-    owner 'root'
-    group 'root'
-    mode '0644'
-  end
+template '/etc/modprobe.d/zfs-arc-max.conf' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  only_if { node['optoro_zfs']['zfs_arc_max'] }
 end
