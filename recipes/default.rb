a7bd1c45d8c6078264e6c87bd10407c6905414ef
Alex Freidah <afreidah@optoro.com>
2015-03-30 15:59:57 -0400

    fixed the order

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,9 @@
-include_recipe 'zfs_linux'
-
+# create limits before installing to avoid having to reload after
 template '/etc/modprobe.d/zfs-arc-max.conf' do
   owner 'root'
   group 'root'
   mode '0644'
   only_if { node['optoro_zfs']['zfs_arc_max'] }
 end
+
+include_recipe 'zfs_linux'
